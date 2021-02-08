81c695af4b647cf1a5efccf7946653c91ec695e7
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-08 11:13:01 -0500

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+  let collection = [];
+  for (let i = 0; i < array.length; i++) {
+    collection.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return collection;
+}
+
+function countDown(num) {
+  let i = 0;
+  while (i <= num) {
+    console.log (num);
+    num--;
+  }
+}
